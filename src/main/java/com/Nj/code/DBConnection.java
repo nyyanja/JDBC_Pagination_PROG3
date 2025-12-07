c7010aacad3f7e5ea9479f639242b6a15a79ef84
nyyanja <nyanja.ramahavatra@gmail>
2025-12-07 11:24:42 +1100

    Add class DBConnection

--- src/main/java/com/Nj/code/DBConnection.java
+++ src/main/java/com/Nj/code/DBConnection.java
@@ -1,4 +1,16 @@
 package com.Nj.code;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private static final String USER = "product_manager_user";
+    private static final String PASSWORD = "123456";
+
+    public Connection getDBConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
+
