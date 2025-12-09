c3606a40556bac64b12843dfd9553a40bf8d799e
nyyanja <nyanja.ramahavatra@gmail>
2025-12-09 13:19:37 +1100

    modified test to remove static

--- src/main/java/com/Nj/code/DBConnection.java
+++ src/main/java/com/Nj/code/DBConnection.java
@@ -5,16 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private final String url;
-    private final String user;
-    private final String password;
+    private final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private final String USER = "product_manager_user";
+    private  final String PASSWORD = "123456";
 
-    public DBConnection(String url, String user, String password) {
-        this.url = url;
-        this.user = user;
-        this.password = password;
-    }
     public Connection getDBConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
