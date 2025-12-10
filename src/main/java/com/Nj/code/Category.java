690368c61a35c2e1560abcd8c72d3059f63e0829
nyyanja <nyanja.ramahavatra@gmail>
2025-12-10 11:25:58 +1100

    Add hasCode and equals on category

--- src/main/java/com/Nj/code/Category.java
+++ src/main/java/com/Nj/code/Category.java
@@ -1,5 +1,7 @@
 package com.Nj.code;
 
+import java.util.Objects;
+
 public class Category {
     private final int id;
     private final String name;
@@ -14,6 +16,19 @@ public class Category {
     @Override
     public String toString() {
         return "Category{id=" + id + ", name='" + name + "'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
 
